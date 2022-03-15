888bdb84d35096e658169b2c1f6f19d6daa2e1c7
ViliusP <viliuspaliokas@gmail.com>
2022-03-15 17:10:28 +0200

    Update script

--- upload.sh
+++ upload.sh
@@ -51,6 +51,7 @@ for i in "${in_arr[@]}"; do
 done
 
 lftp -e "set xfer:log 1; \
+  set sftp:auto-confirm yes; \
   set ftp:ssl-allow $PLUGIN_SECURE; \
   set ftp:ssl-force $PLUGIN_SECURE; \
   set ftp:ssl-protect-data $PLUGIN_SECURE; \
@@ -59,4 +60,4 @@ lftp -e "set xfer:log 1; \
   set net:max-retries 3; \
   $PLUGIN_CLEAN_DIR; \
   mirror --verbose $PLUGIN_CHMOD -R $PLUGIN_MIRROR_OPTS $PLUGIN_INCLUDE_STR $PLUGIN_EXCLUDE_STR $(pwd)$PLUGIN_SRC_DIR $PLUGIN_DEST_DIR" \
-  -u $FTP_USERNAME,$FTP_PASSWORD $PLUGIN_HOSTNAME
+  -u $FTP_USERNAME,$FTP_PASSWORD $PLUGIN_HOSTNAME << EOF
